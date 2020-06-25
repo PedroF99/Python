2ae5d7f65839f063ad5545088f000572d488526a
PedroF99 <pedrofilipeferreira10i@gmail.com>
2020-06-25 16:05:47 +0100

    semana

---
+++ Array completo.py
@@ -0,0 +1,29 @@
+nome = ["Pedro", "João", "Ana", "Miguel"]
+
+n = nome[0]
+
+print(n)
+
+nome.append("Maria")
+
+nome.pop(1)
+
+nome.remove("Miguel")
+
+b = nome[3]
+
+print(b)
+
+
+
+#append()	Adiciona um elemento no final da lista
+#clear()	Remove todos os elementos da lista
+#copy()	    Retorna uma cópia da lista
+#count()	Retorna o número de elementos com o valor especificado
+#extend()	Adiciona os elementos de uma lista (ou qualquer iterável) ao final da lista atual
+#index()	Retorna o índice do primeiro elemento com o valor especificado
+#insert()	Adiciona um elemento na posição especificada
+#pop()	    Remove o elemento na posição especificada
+#remove()	Remove o primeiro item com o valor especificado
+#reverse()	Inverte a ordem da lista
+#sort()	    Classifica a lista
---
+++ Subprogramas e Array´s.py
@@ -0,0 +1,34 @@
+#n=2
+#e=3
+
+#def ola():
+#    print(n+e)
+    
+
+
+#ola()
+
+
+
+#def hello(meu_nome):
+#    print('Olá' ,meu_nome)
+    
+    
+#hello('Pedro')
+
+
+
+nome = ["Pedro", "João", "Ana"]
+
+n = nome[0]
+
+print(n)
+
+
+
+
+
+
+
+
+
